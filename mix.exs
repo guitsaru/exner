689bbebf174b68fceb4361f7a271ec12a498d66a
Matt Pruitt <guitsaru@gmail.com>
2020-10-03 22:18:09 +0700

    Add GitHub Actions

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Exner.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.github": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -22,7 +29,8 @@ defmodule Exner.MixProject do
   defp deps do
     [
       {:credo, "~> 1.4.0", only: [:dev, :test]},
-      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.13.2", only: [:test], runtime: false}
     ]
   end
 end
